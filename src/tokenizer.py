e8ac777486e051ee813fcbef2ea8503054c9e424
Marcus Carpenter <marcus.carpenter97@yahoo.co.uk>
2025-11-30 15:04:20 -0300

    Add functionality of setting text to lowercase when tokenizing.

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -4,7 +4,8 @@ from collections import Counter
 class Tokenizer:
 
     def tokenize(self, text):
-        no_punctuation = text.translate(str.maketrans("", "", string.punctuation))
+        lower_text = text.lower()
+        no_punctuation = lower_text.translate(str.maketrans("", "", string.punctuation))
         tokens = no_punctuation.split(" ")
         return [token for token in tokens if len(token) > 1]
 
