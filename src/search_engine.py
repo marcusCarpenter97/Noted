51d57cbd3ad2378cff0a3ab4cfca34d2c431a90b
Marcus Carpenter <marcus.carpenter97@yahoo.co.uk>
2025-11-30 14:22:16 -0300

    Implement function to search for notes based on a user query.

--- src/search_engine.py
+++ src/search_engine.py
@@ -1,4 +1,5 @@
 import logging 
+from collections import defaultdict
 from notes_repository import NotesRepository
 
 class SearchEngine:
@@ -27,3 +28,19 @@ class SearchEngine:
 
     def remove_from_index(self, note_id):
         self.notes_index.delete_tokens_for_note(note_id)
+
+    def search(self, user_query):
+        query_tokens = self.tokenizer.tokenize(user_query)
+
+        result_scores = defaultdict(int)
+
+        for token in query_tokens:
+            similar_tokens = self.notes_index.retrieve_similar_tokens(token)
+
+            for note_id, count in similar_tokens:
+                if note_id not in result_scores:
+                    result_scores[note_id] = 0
+                result_scores[note_id] += count
+
+        final_result = list(result_scores.items())
+        return sorted(final_result, key=lambda x: x[1])
