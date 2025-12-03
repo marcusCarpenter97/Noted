b309e650d09a6af0a1837bd73e40de869ab4f931
Marcus Carpenter <marcus.carpenter97@yahoo.co.uk>
2025-12-03 09:30:59 -0300

    Fix typo in fixture names.

--- src/remote_api_client.py
+++ src/remote_api_client.py
@@ -6,9 +6,9 @@ class RemoteAPIClient:
         self.fixtures_dir = Path(fixtures_dir)
 
     def pull_changes(self, since_timestamp: str) -> list[dict]:
-        with open(self.fixtures_dir / "pull_responce.json") as f:
+        with open(self.fixtures_dir / "pull_response.json") as f:
             return json.load(f)
 
     def push_changes(self, notes: list[dict]) -> dict:
-        with open(self.fixtures_dir / "push_responce.json") as f:
+        with open(self.fixtures_dir / "push_response.json") as f:
             return json.load(f)
