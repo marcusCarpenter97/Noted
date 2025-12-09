ba9719435aec51cda5f693a2aba39f957f72cf8e
Marcus Carpenter <marcus.carpenter97@yahoo.co.uk>
2025-12-09 08:54:27 -0300

    Implement device identification with uuids and public/private keys.

---
+++ src/device_identification.py
@@ -0,0 +1,47 @@
+
+from nacl.signing import SigningKey
+import uuid
+
+class DeviceID:
+    def __init__(self, db):
+        self.db = db
+
+    def get_or_generate_device_id(self):
+        cursor = self.db.get_database_cursor()
+        cursor.execute("CREATE TABLE IF NOT EXISTS device_id(uuid TEXT PRIMARY KEY)")
+
+        cursor.execute("SELECT uuid FROM device_id")
+        row = cursor.fetchone()
+
+        if row:
+            return row[0]
+
+        device_id = str(uuid.uuid4())
+        cursor.execute("INSERT INTO device_id (uuid) VALUES (?)", (device_id,))
+        self.db.commit_to_database()
+
+        return device_id
+
+    def get_or_generate_public_private_keys(self):
+        cursor = self.db.get_database_cursor()
+
+        cursor.execute("""CREATE TABLE IF NOT EXISTS keys(
+                        name TEXT PRIMARY KEY,
+                        private_key BLOB,
+                        public_key BLOB)""")
+
+        cursor.execute("SELECT private_key, public_key FROM keys WHERE name='p2p'")
+        row = cursor.fetchone()
+
+        if row:
+            private = SigningKey(row[0])
+            public = private.verify_key
+            return private, public
+
+        private = SigningKey.generate()
+        public = private.verify_key
+
+        cursor.execute("INSERT INTO keys VALUES (?, ?, ?)", ("p2p", private.encode(), public.encode()))
+        self.db.commit_to_database()
+
+        return private, public
