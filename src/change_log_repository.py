487233847173901cb02885addf59d711675baeef
Marcus Carpenter <marcus.carpenter97@yahoo.co.uk>
2025-12-11 12:00:41 -0300

    Implements the check_operation_exists method in change log.

--- src/change_log_repository.py
+++ src/change_log_repository.py
@@ -33,3 +33,17 @@ class ChangeLog:
                 VALUES (?, ?, ?, CURRENT_TIMESTAMP, ?, ?, ?, ?)""",
                 (op_id, note_id, operation_type, device_id, json.dumps(payload), lamport_timestamp, origin_device))
         self.db.commit_to_database()
+
+    def check_operation_exists(self, operation_id):
+        cursor = self.db.get_database_cursor()
+        cursor.execute("SELECT EXISTS (SELECT 1 FROM change_log WHERE op_id = ?) AS value_exists", (operation_id,))
+        return cursor.fetchone()['value_exists']
+
+if __name__ == "__main__":
+    from database import Database
+
+    db = Database(":memory:")
+    cl = ChangeLog(db)
+    cl.create_change_log_table()
+
+    print(cl.check_operation_exists("abcd"))
