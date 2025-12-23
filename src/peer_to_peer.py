442aa6ced2649d7684b354816aeaaee72b638c3a
Marcus <marcus.carpenter97@yahoo.co.uk>
2025-12-23 10:12:51 -0300

    Implement secure device discovery via the advertisement of public keys.

--- src/peer_to_peer.py
+++ src/peer_to_peer.py
@@ -2,6 +2,7 @@ from zeroconf import Zeroconf, ServiceInfo, ServiceBrowser
 import base64
 import socket
 import logging
+from cryptography.hazmat.primitives import serialization
 
 class ServiceListener:
 
@@ -10,7 +11,6 @@ class ServiceListener:
         self.transport_layer = transport_layer
 
     def add_service(self, zeroconf, service_type, name):
-        logging.info(f"[+] Service added: {name}")
         info = zeroconf.get_service_info(service_type, name)
 
         if not info:
@@ -23,12 +23,13 @@ class ServiceListener:
 
         properties["peer_ip"] = socket.inet_ntoa(info.addresses[0])
         properties["peer_port"] = info.port
+        properties["zeroconf_name"] = name
 
         self.transport_layer.register_new_peer(properties)
 
     def remove_service(self, zeroconf, service_type, name):
         logging.info(f"[-] Service removed: {name}")
-        print(f"Device {name} disconected")
+        self.transport_layer.remove_service(name)
 
     def update_service(self, zeroconf, service_type, name):
         logging.info(f"[~] Service updated: {name}")
@@ -38,7 +39,7 @@ class ServiceListener:
         for k, v in d.items():
             key = k.decode() if isinstance(k, bytes) else k
             if key == "public_key":
-                decoded[key] = v
+                decoded[key] = base64.b64decode(v)
             else:
                 decoded[key] = v.decode() if isinstance(v, bytes) else v
         return decoded
@@ -63,11 +64,18 @@ def advertise(device_id, public_key, device_name):
     ip_address = socket.inet_aton(get_default_ip())
     port = 5000
 
+    public_key_bytes = public_key.public_bytes(encoding=serialization.Encoding.DER,
+                                               format=serialization.PublicFormat.SubjectPublicKeyInfo)
+
+    public_key_b64 = base64.b64encode(public_key_bytes).decode("ascii")
+
     properties = {
         "device_id": device_id,
-        "public_key": base64.b64encode(public_key).decode("ascii"),
+        "public_key": public_key_b64,
         "device_name": device_name
     }
+
+    logging.info(f"Advertising with public key {public_key_b64}")
 
     info = ServiceInfo(
             type_=service_type,
