3daa540a8c25cc9d4f61005eca7e9b8794b85193
Marcus <marcus.carpenter97@yahoo.co.uk>
2025-12-24 15:37:16 -0300

    Refactor sync manager to make use of new asyncronous database access.

--- src/sync_manager.py
+++ src/sync_manager.py
@@ -1,3 +1,4 @@
+import json
 import logging
 import pickle
 from change_log_repository import ChangeLog
@@ -6,8 +7,8 @@ from remote_api_client import RemoteAPIClient
 
 class SyncManager:
 
-    def __init__(self, db, device_id, notes_repository, change_log, lamport_clock, se, li, fe, ep, transport_layer):
-        self.db = db
+    def __init__(self, db_worker, device_id, notes_repository, change_log, lamport_clock, se, li, fe, ep, transport_layer):
+        self.db_worker = db_worker
         self.device_id = device_id
         self.notes_repo = notes_repository
         self.change_log = change_log
@@ -22,26 +23,34 @@ class SyncManager:
         self.transport_layer.register_message_handler(self.sync_down)
 
     def create_last_sync_table(self):
-        cursor = self.db.get_database_cursor()
-        cursor.execute("""CREATE TABLE IF NOT EXISTS last_sync (
-                            id INTEGER PRIMARY KEY CHECK (id = 1) DEFAULT 1,
-                            last_updated DATETIME)""")
+        def _op(connection):
+            cursor = connection.cursor()
+            cursor.execute("""CREATE TABLE IF NOT EXISTS last_sync (
+                                id INTEGER PRIMARY KEY CHECK (id = 1) DEFAULT 1,
+                                last_updated DATETIME)""")
+        self.db_worker.execute(_op)
 
     def initialize_sync_table(self):
-        # Insert if empty.
-        cursor = self.db.get_database_cursor()
-        cursor.execute("""INSERT INTO last_sync (last_updated) SELECT CURRENT_TIMESTAMP WHERE NOT EXISTS (SELECT * FROM last_sync)""")
-        self.db.commit_to_database()
+        def _op(connection):
+            cursor = connection.cursor()
+            # Insert if empty.
+            cursor.execute("""INSERT INTO last_sync (last_updated) SELECT CURRENT_TIMESTAMP WHERE NOT EXISTS (SELECT * FROM last_sync)""")
+            connection.commit()
+        self.db_worker.execute(_op)
 
     def update_last_sync(self):
-        cursor = self.db.get_database_cursor()
-        cursor.execute("UPDATE last_sync SET last_updated = CURRENT_TIMESTAMP WHERE id = 1")
-        self.db.commit_to_database()
+        def _op(connection):
+            cursor = connection.cursor()
+            cursor.execute("UPDATE last_sync SET last_updated = CURRENT_TIMESTAMP WHERE id = 1")
+            connection.commit()
+        self.db_worker.execute(_op)
 
     def get_last_sync(self):
-        cursor = self.db.get_database_cursor()
-        cursor.execute("SELECT last_updated FROM last_sync WHERE id = 1")
-        return cursor.fetchone()[0]
+        def _op(connection):
+            cursor = connection.cursor()
+            cursor.execute("SELECT last_updated FROM last_sync WHERE id = 1")
+            return cursor.fetchone()[0]
+        return self.db_worker.execute(_op, wait=True)
 
     def sync_up(self, batch_size=50):
         """ Send new changes to the server. """
@@ -61,6 +70,7 @@ class SyncManager:
         """ Pull new changes from the server. """
         last_sync_at = self.get_last_sync()
 
+        logging.info(f"Received from peer {message}")
         logging.info(f"Received {len(message)} notes from {peer_device_id}")
 
         results = sorted(message, key=lambda x: x.get('lamport_clock', 0))
@@ -70,7 +80,10 @@ class SyncManager:
             if self.change_log.check_operation_exists(remote_operation['op_id']) == 1:
                 continue
 
-            local_note = self.notes_repo.get_note(remote_operation['uuid'])
+            local_note = self.notes_repo.get_note(remote_operation['note_id'])
+
+            # Payload comes as a string and need to be convereted to a dictionary.
+            remote_operation["payload"] = json.loads(remote_operation["payload"])
 
             if remote_operation['operation_type'] == 'create':
                 if local_note is None:
@@ -82,66 +95,65 @@ class SyncManager:
 
                     embeddings = pickle.dumps(response['embedding'])
 
-                    self.notes_repo.insert_note(remote_operation['uuid'], remote_operation['payload']['title'],
+                    self.notes_repo.insert_note(remote_operation['note_id'], remote_operation['payload']['title'],
                                                 remote_operation['payload']['contents'], remote_operation['payload']['created_at'],
                                                 remote_operation['payload']['last_updated'],
                                                 embeddings, remote_operation['payload']['tags'])
 
-                    self.search_engine.index_note(remote_operation['uuid'])
+                    self.search_engine.index_note(remote_operation['note_id'])
 
-                    self.lexical_index.index_note_for_lexical_search(remote_operation['uuid'],
+                    self.lexical_index.index_note_for_lexical_search(remote_operation['note_id'],
                                                                     remote_operation['payload'].get('title', ''),
                                                                     remote_operation['payload'].get('contents', ''))
-                    self.faiss_engine.add_embedding(remote_operation['uuid'], response['embedding'])
+                    self.faiss_engine.add_embedding(remote_operation['note_id'], response['embedding'])
 
-                    self.change_log.log_operation(remote_operation['uuid'],
+                    self.change_log.log_operation(remote_operation['op_id'],
                                                     "create", remote_operation['payload'], self.lamport_clock.now(),
                                                     self.device_id)
-                    logging.info(f"Inserted note from remote with id: {remote_operation['uuid']}")
+                    logging.info(f"Inserted note from remote with id: {remote_operation['note_id']}")
                     self.update_last_sync()
                 else:
-                    logging.warning(f"Could not create note because a note with this id already exists. Note id : {remote_operation['uuid']}")
+                    logging.warning(f"Could not create note because a note with this id already exists. Note id : {remote_operation['note_id']}")
 
             if remote_operation['operation_type'] == 'update':
                 if local_note is not None:  # Use .get because parameters may not exist in update function.
                     self.lamport_clock.increment_lamport_time(remote_operation['lamport_clock'])
                     self.lamport_clock.save_lamport_time_to_db()
-                    self.notes_repo.update_note(remote_operation['uuid'],
+                    self.notes_repo.update_note(remote_operation['note_id'],
                                                 remote_operation['payload'].get('title', None),
                                                 remote_operation['payload'].get('contents', None),
                                                 remote_operation['payload'].get('embeddings', None),
                                                 remote_operation['payload'].get('tags', None))
 
-                    self.search_engine.index_note(remote_operation['uuid'])
+                    self.search_engine.index_note(remote_operation['note_id'])
 
-                    note = self.notes_repo.get_note(remote_operation['uuid'])
-                    self.lexical_index.index_note_for_lexical_search(note['uuid'], note['title'], note['contents'])
+                    note = self.notes_repo.get_note(remote_operation['note_id'])
+                    self.lexical_index.index_note_for_lexical_search(note['note_id'], note['title'], note['contents'])
 
                     response = self.embedding_provider.embed(f"{note['title']} {note['contents']} {note['tags']}")
-                    self.faiss_engine.add_embedding(remote_operation['uuid'], response['embedding'])
+                    self.faiss_engine.add_embedding(remote_operation['note_id'], response['embedding'])
 
-                    self.change_log.log_operation(remote_operation['uuid'],
+                    self.change_log.log_operation(remote_operation['op_id'],
                                                     "update", remote_operation['payload'], self.lamport_clock.now(),
                                                     self.device_id)
-                    logging.info(f"Succesfully updated note with id: {remote_operation['uuid']}")
+                    logging.info(f"Succesfully updated note with id: {remote_operation['note_id']}")
                     self.update_last_sync()
                 else:
-                    logging.warning(f"Could not update note becuase a note with this id does not exist. Note id : {remote_operation['uuid']}")
+                    logging.warning(f"Could not update note becuase a note with this id does not exist. Note id : {remote_operation['note_id']}")
 
             if remote_operation['operation_type'] == 'delete':
                 if local_note is not None:
                     self.lamport_clock.increment_lamport_time(remote_operation['lamport_clock'])
                     self.lamport_clock.save_lamport_time_to_db()
-                    self.notes_repo.mark_note_as_deleted(remote_operation['uuid'])
-                    self.lexical_index.delete_note_from_lexical_search(remote_operation['uuid'])
-                    self.search_engine.remove_from_index(remote_operation['uuid'])
-                    self.faiss_engine.delete_embedding(remote_operation['uuid'])
-                    self.change_log.log_operation(remote_operation['uuid'], "delete", {'deleted': 1}, self.lamport_clock.now(), self.device_id)
-                    logging.info(f"Marked note for deletion with id: {remote_operation['uuid']}")
+                    self.notes_repo.mark_note_as_deleted(remote_operation['note_id'])
+                    self.lexical_index.delete_note_from_lexical_search(remote_operation['note_id'])
+                    self.search_engine.remove_from_index(remote_operation['note_id'])
+                    self.faiss_engine.delete_embedding(remote_operation['note_id'])
+                    self.change_log.log_operation(remote_operation['op_id'], "delete", {'deleted': 1}, self.lamport_clock.now(), self.device_id)
+                    logging.info(f"Marked note for deletion with id: {remote_operation['note_id']}")
                     self.update_last_sync()
                 else:
-                    logging.warning(f"Could not delete note becuase a note with this id does not exist. Note id : {remote_operation['uuid']}")
+                    logging.warning(f"Could not delete note becuase a note with this id does not exist. Note id : {remote_operation['note_id']}")
 
     def sync(self):
         self.sync_up()
-        #self.sync_down()
