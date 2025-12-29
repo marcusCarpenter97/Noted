51896282601d9e903a78749cc82d6382e349d72d
Marcus <marcus.carpenter97@yahoo.co.uk>
2025-12-28 21:51:07 -0300

    Implement a hard limit on the size of the text given to the embedding model preventing an error from occuring.

--- src/embedding_provider.py
+++ src/embedding_provider.py
@@ -2,5 +2,5 @@ import ollama
 
 class EmbeddingProvider:
 
-    def embed(self, text, model="nomic-embed-text"):
-        return ollama.embeddings(model=model, prompt=text)
+    def embed(self, text, model="nomic-embed-text", max_chars=5000):
+        return ollama.embeddings(model=model, prompt=text[:max_chars])
